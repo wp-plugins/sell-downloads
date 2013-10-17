de9b195c90712e932d70900c74402145c51cf4d0
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-17 21:28:59 +0000

    The current update corrects the queries to database in the Sell Downloads.
    git-svn-id: https://plugins.svn.wordpress.org/sell-downloads/trunk@789519 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sell-downloads.php
+++ sell-downloads.php
@@ -172,27 +172,32 @@ Description: Sell Downloads is an online store for selling downloadable files: a
 		} // End init
 		
         function _sd_create_pages( $slug, $title ){
+            if( session_id() == "" ) session_start();
+			if( isset( $_SESSION[ $slug ] ) ) return $_SESSION[ $slug ];
+            
             $page = get_page_by_path( $slug ); 
-            if( is_admin() ){
-                if( is_null( $page ) ){
-                    if( wp_insert_post(
-                            array(
-                                'comment_status' => 'closed',
-                                'post_name' => $slug,
-                                'post_title' => __( $title, SD_TEXT_DOMAIN ),
-                                'post_status' => 'publish',
-                                'post_type' => 'page'
-                            )
-                        )    
-                    ){
-                        $page = get_page_by_path( $slug ); 
-                    }
-                }else{
-                    $page->post_status = 'publish';
-                    wp_update_post( $page );
-                }
-            }    
-            return ( !is_null( $page ) ) ? get_permalink($page->ID) : SD_H_URL;
+			if( is_null( $page ) ){
+				if( is_admin() ){
+					if( false != ($id = wp_insert_post(
+								array(
+									'comment_status' => 'closed',
+									'post_name' => $slug,
+									'post_title' => __( $title, SD_TEXT_DOMAIN ),
+									'post_status' => 'publish',
+									'post_type' => 'page'
+								)
+							)
+						)    
+					){
+						$_SESSION[ $slug ] =  get_permalink($id);
+					}
+				}    
+			}else{
+				$_SESSION[ $slug ] =  get_permalink($page->ID);
+			}	
+			
+            $_SESSION[ $slug ] = ( isset( $_SESSION[ $slug ] ) ) ? $_SESSION[ $slug ] : SD_H_URL;
+            return $_SESSION[ $slug ];
         }
         
         function _sd_exclude_pages( $pages ){
