0e0ad732872f49d076bc57c88e344d964311f26f
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-13 23:06:22 +0000

    The current update includes the date as a column in the sales reports to identify the sales easier.
    git-svn-id: https://plugins.svn.wordpress.org/sell-downloads/trunk@1006891 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sell-downloads.php
+++ sell-downloads.php
@@ -1508,7 +1508,7 @@ Description: Sell Downloads is an online store for selling downloadable files: a
 					$_group  = "";
 					$_order  = "";
 					$_date_dif = floor( max( abs( strtotime( $to_year.'-'.$to_month.'-'.$to_day ) - strtotime( $from_year.'-'.$from_month.'-'.$from_day ) ) / ( 60*60*24 ), 1 ) );
-					$_table_header = array( 'Product', 'Buyer', 'Amount', 'Currency', 'Download link', '' );
+					$_table_header = array( 'Date', 'Product', 'Buyer', 'Amount', 'Currency', 'Download link', '' );
 					
 					if( $group_by == 'no_group' )	
 					{
@@ -1698,6 +1698,7 @@ Description: Sell Downloads is an online store for selling downloadable files: a
 												
 												echo '
 													<TR>
+														<TD>'.$purchase->date.'</TD>
 														<TD><a href="'.get_permalink($purchase->ID).'" target="_blank">'.$purchase->post_title.'</a></TD>
 														<TD>'.$purchase->email.'</TD>
 														<TD>'.$purchase->amount.'</TD>
