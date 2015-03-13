361b1aa1aea5289b0e459cea604df1b8194b6991
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-13 13:02:30 +0000

    The current update:
    * Includes the Featured Image as part of products, because some plugins and themes use the featured Images on post types.
    * Uses functions included since WordPress 3.5.0, that modifies the plugin requirements.
    
    git-svn-id: https://plugins.svn.wordpress.org/sell-downloads/tags/1.0.3@1111917 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sd-script/sd-public.js
@@ -0,0 +1,92 @@
+jQuery(function($){
+	
+	$('.sell-downloads-tabs').click(function(evt){
+		var m = $(this),
+			t = $(evt.target);
+			
+		m.find('.active-tab').removeClass('active-tab');
+		t.addClass('active-tab');
+		$('.sell-downloads-tabs-container').removeClass('active-tab').eq(m.children().index(t)).addClass('active-tab');
+	});
+	
+	// ****** FUNCTIONS FOR DEMO ****** //
+	$( document ).on( 'click', '.sd-demo-close', function( evt ){
+		evt.preventDefault();
+		
+		var e  = $( evt.target );
+		    c  = e.parents( '.sd-demo-container' ),
+			sl = c.attr( 'sl' ),
+			st = c.attr( 'st' );
+
+		c.remove();
+		$( 'body,html' ).removeClass( 'sd-demo' ).scrollLeft( sl ).scrollTop( st );
+	} );
+	
+	$( window ).on( 'resize', function(){
+		var b  = $( 'body' ),
+		    h  = b.height();
+		
+		b.find( '#sd_demo_object' ).attr( 'height', ( h - b.find( '.sd-demo-head' ).height() ) + 'px' );
+	} );
+	
+	$( '.sd-demo-link' ).click(function( evt ){
+		evt.preventDefault();
+		
+		var e  = $( evt.target ),
+		    m  = $(e).attr( 'mtype' ),
+			l  = e.attr( 'href' ),
+			i  = l.indexOf( 'file=' ),
+			t  = $( 'html' ),
+			b  = $( 'body' ),
+			close_txt = ( typeof sd_global != 'undefined' && typeof sd_global.texts != 'undefined' && typeof sd_global.texts.close_demo != 'undefined' ) ? sd_global.texts.close_demo : 'close',
+			download_txt = ( typeof sd_global != 'undefined' && typeof sd_global.texts != 'undefined' && typeof sd_global.texts.download_demo != 'undefined' ) ? sd_global.texts.download_demo : 'download file',
+			plugin_fault_txt = ( typeof sd_global != 'undefined' && typeof sd_global.texts != 'undefined' && typeof sd_global.texts.plugin_fault != 'undefined' ) ? sd_global.texts.plugin_fault : 'The Object to display the demo file is not enabled in your browser. CLICK HERE to download the demo file',
+			sl = b.scrollLeft(),
+			st = b.scrollTop();
+			
+		l = decodeURIComponent( l.substr( i+5 ) );
+		
+		t.addClass( 'sd-demo' );
+		b.addClass( 'sd-demo' );
+		
+		var h = $( window ).height();
+
+		b.append( $( '<div class="sd-demo-container" sl="'+sl+'" st="'+st+'" style="height:100%;width:100%;position:absolute;top:0;left:0;z-index:99999;">\
+		                <div class="sd-demo-head">\
+						  <a href="'+e.attr( 'href' )+'">'+download_txt+'</a>\
+						  <a href="#" class="sd-demo-close">'+close_txt+'</a>\
+						</div>\
+					    <div class="sd-demo-body">\
+			              <object id="sd_demo_object" data="'+l+'" type="'+m+'" width="100%"> \
+						     <div style="margin-top:40px;">\
+				             <a href="'+e.attr( 'href' )+'">'+plugin_fault_txt+'</a>\
+							 </div>\
+			              </object>\
+					    </div>\
+					  </div>' ) );
+		
+		b.find( '#sd_demo_object' ).attr( 'height', ( h - b.find( '.sd-demo-head' ).height() ) + 'px' );
+	});
+	
+	$( '.sd-demo-media' ).mediaelementplayer();
+	timeout_counter = 10;
+    window['sell_downloads_counting'] = function()
+    {
+        var loc = document.location.href;
+        document.getElementById( "sell_downloads_error_mssg" ).innerHTML = timeout_text+' '+timeout_counter;
+        if( timeout_counter == 0 )
+        {
+            document.location = loc+( ( loc.indexOf( '?' ) == -1 ) ? '?' : '&' )+'timeout=1';    
+        }
+        else
+        {
+            timeout_counter--;
+            setTimeout( sell_downloads_counting, 1000 );
+        }    
+    };
+    
+    if( $( '[id="sell_downloads_error_mssg"]' ).length ) 
+    {
+        sell_downloads_counting();
+    }
+});
