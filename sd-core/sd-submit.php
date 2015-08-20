40382e237aa01eca23f1d1c02b91ea2b1bd8d068
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-20 19:54:52 +0000

    The curent update:
    * Allows to enable/disable the PayPal Sandbox in the store's settings for testing the purchases
    
    git-svn-id: https://plugins.svn.wordpress.org/sell-downloads/tags/1.0.13@1226502 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sd-core/sd-submit.php
@@ -0,0 +1,60 @@
+<?php
+
+if( !defined( 'SD_H_URL' ) ) { echo 'Direct access not allowed.';  exit; }
+
+    $ms_paypal_email = get_option('sd_paypal_email');
+
+	$baseurl = SD_H_URL.'?sd_action=ipn';
+	$returnurl = $GLOBALS['sell_downloads']->_sd_create_pages( 'sd-download-page', 'Download the purchased products' );
+    $returnurl .= ( strpos( $returnurl, '?' ) === false ) ? '?' : '&';
+	
+	if( preg_match( '/^(http(s)?:\/\/[^\/\n]*)/i', SD_H_URL, $matches ) && strpos( $_SERVER['HTTP_REFERER'], $matches[ 0 ] ) ) $cancel_url = $_SERVER['HTTP_REFERER'];
+    if(empty($cancel_url)) $cancel_url = SD_H_URL;
+            
+    if($ms_paypal_email){ // Check for sealer email
+        mt_srand(sell_downloads_make_seed());
+        $randval = mt_rand(1,999999);
+            
+        $purchase_id = md5($randval.uniqid('', true));
+        
+        if(isset($_POST['sd_product_id'])){
+            $product = $wpdb->get_row($wpdb->prepare("SELECT posts_data.id as id, posts_data.price as price, posts.post_title as title FROM ".$wpdb->prefix.SDDB_POST_DATA." as posts_data INNER JOIN ".$wpdb->prefix."posts as posts ON posts.ID = posts_data.id WHERE posts_data.id=%d AND posts.post_status='publish' AND posts.post_type='sd_product'", $_POST['sd_product_id']));
+            
+            if($product){
+                $amount = $product->price;
+                $title = $product->title;
+                $number = $product->id;
+                $ID = $product->id;
+            }else{
+                $price = 0;
+            }    
+        }    
+        
+        if($amount > 0){
+            $code = '<form action="https://www.'.( ( get_option( 'sd_paypal_sandbox', false ) ) ? 'sandbox.' : '' ).'paypal.com/cgi-bin/webscr" name="ppform'.$randval.'" method="post">'.
+			'<input type="hidden" name="business" value="'.$ms_paypal_email.'" />'.
+            '<input type="hidden" name="item_name" value="'.$title.'" />'.
+            '<input type="hidden" name="item_number" value="Item Number '.$number.'" />'.
+            '<input type="hidden" name="amount" value="'.$amount.'" />'.
+            '<input type="hidden" name="currency_code" value="'.get_option('sd_paypal_currency', SD_PAYPAL_LANGUAGE).'" />'.
+            '<input type="hidden" name="lc" value="'.get_option('sd_paypal_language', SD_PAYPAL_LANGUAGE).'" />'.
+            ''.
+            '<input type="hidden" name="return" value="'.$returnurl.'&purchase_id='.$purchase_id.'" />'.
+            '<input type="hidden" name="cancel_return" value="'.$cancel_url.'" />'.
+            '<input type="hidden" name="notify_url" value="'.$baseurl.'&id='.$ID.'&purchase_id='.$purchase_id.'&rtn_act=purchased_product_sell_downloads" />'.
+            ''.
+            '<input type="hidden" name="cmd" value="_xclick" />'.
+            '<input type="hidden" name="page_style" value="Primary" />'.
+            '<input type="hidden" name="no_shipping" value="1" />'.
+            '<input type="hidden" name="no_note" value="1" />'.
+            '<input type="hidden" name="bn" value="NetFactorSL_SI_Custom" />'.
+            '<input type="hidden" name="ipn_test" value="1" />'.
+            '</form>'.
+            '<script type="text/javascript">document.ppform'.$randval.'.submit();'.'</script>';
+            echo $code;
+            exit;
+        }
+    }   
+	
+	header('location: '.$cancel_url);
+?>
---
+++ sd-layouts/02/script.js
@@ -0,0 +1,138 @@
+jQuery( function( $ )
+	{
+		// Correct the header and items width
+		var correct_header = function()
+			{
+				$( '.sell-downloads-items,.sell-downloads-pagination' ).each(
+					function()
+					{
+
+						var e = $( this );
+						if( e.parents( '.widget' ).length == 0 )
+						{
+							e.css( 'width', $( '.sell-downloads-header' ).outerWidth( ) );
+						}
+					}
+				);
+			};
+		
+		correct_header();
+		
+		// Replace the popularity texts with the stars 
+		var popularity_top = 0;
+		$( '.product-popularity' ).each(
+			function()
+			{
+				var e = $( this ),
+					p = parseInt( e.find( 'span' ).remove().end().text().replace( /\s/g, '' ) );
+					
+				e.text( '' ).attr( 'popularity', p );
+				popularity_top = Math.max( popularity_top, p );
+			}
+		);
+		
+		$( '.product-popularity' ).each(
+			function()
+			{
+			
+				var e = $( this ),
+					p = e.attr( 'popularity' ),
+					str = '',
+					active = 0;
+
+				if( popularity_top > 0 )
+				{
+					active = Math.ceil( p / popularity_top * 100 / 20 );
+				}
+				
+				for( var i = 0; i < active; i++ )
+				{
+					str += '<div class="star-active"></div>';
+				}
+				
+				for( var i = 0, h = 5 - active; i < h; i++ )
+				{
+					str += '<div class="star-inactive"></div>';
+				}
+				e.html( str );
+			}
+		);
+		
+		// Correct the images heights
+		var min_height = Number.MAX_VALUE
+			correct_heights = function()
+			{
+			$( '.sell-downloads-items .product-cover img' ).each(
+				function()
+				{
+					var e = $( this );
+					min_height = Math.min( e.height(), min_height );
+				}
+			);
+
+			if( min_height != Number.MAX_VALUE )
+			{
+				$( '.sell-downloads-items .product-cover' ).css( { 'height': min_height+'px', 'overflow': 'hidden' } );
+			}	
+			
+			$( '.product-cover' ).append( $( '<div class="sd-inner-shadow"></div>' ) );
+			
+			// Correct the item heights
+			var	height_arr = [],
+				max_height = 0;
+			$( '.sell-downloads-items' ).children( 'div' ).each(
+				function()
+				{
+					var e = $( this );
+					if( e.hasClass( 'sell-downloads-item' ) )
+					{
+						max_height = Math.max( e.height(), max_height );
+					}
+					else
+					{
+						height_arr.push( max_height );
+						max_height = 0;
+					}
+				}
+			);
+			
+			if( height_arr.length )
+			{
+				$( '.sell-downloads-items' ).children( 'div' ).each(
+					function()
+					{
+						var e = $( this );
+						if( e.hasClass( 'sell-downloads-item' ) )
+						{
+							e.height( height_arr[ 0 ] );
+						}
+						else
+						{
+							height_arr.splice( 0, 1 );
+						}
+					}
+				);
+			}	
+		};
+		
+		$( window ).load( correct_heights );
+		
+		// Modify the price box
+		$( '.product-price' ).each(
+			function()
+			{
+				var e = $( this );
+				e.closest( 'div' ).addClass( 'price-box' ).find( 'span:not(.product-price),span.invalid' ).remove();
+			}
+		);
+		
+		// Modify the single pages structure
+		$( '.sell-downloads-song .left-column' ).append( $('<div></div>').html( $( '.sell-downloads-song .right-column' ).html() ) );
+		$( '.sell-downloads-song .right-column' ).html( '' ).append( $( '.sell-downloads-song .bottom-content' ) );
+		$( '.sell-downloads-collection .left-column' ).append( $('<div></div>').html( $( '.sell-downloads-collection .right-column' ).html() ) );
+		$( '.sell-downloads-collection .right-column' ).html( '' ).append( $( '.sell-downloads-collection .bottom-content' ) );
+		
+		// Modify the shopping cart design
+		$( '.sd-shopping-cart-list,.sd-shopping-cart-resume' ).wrap( '<div class="sd-shopping-cart-wrapper"></div>' );
+	} 	
+);
